86f15a26070d2e55b41be0f436d4adcb4065884f
David Vera Castillo <indigodavid@gmail.com>
2022-07-18 14:00:06 -0500

    Fix linters errors

--- src/pages/MissionsPage.js
+++ src/pages/MissionsPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const MissionsPage = () => {
-  const missions = useSelector(state => state.missions);
+  const missions = useSelector((state) => state.missions);
   return (
     <div className="missions">
       <header>
@@ -11,22 +11,19 @@ const MissionsPage = () => {
         <h2>Status</h2>
       </header>
       <ul className="missionsData">
-        {missions.map((mission) => {
-          const { mission_name, description, mission_id } = mission;
-          return (
-            <li key={mission_id}>
-              <div className="missionName">{mission_name}</div>
-              <div className="description">{description}</div>
-              <div className="status">
-                <button className="membership">Not a Member</button>
-                <button className="join">Join Mission</button>
-              </div>
-            </li>
-          )
-        })}
+        {missions.map((mission) => (
+          <li key={mission.mission_id}>
+            <div className="missionName">{mission.mission_name}</div>
+            <div className="description">{mission.description}</div>
+            <div className="status">
+              <button type="button" className="membership">Not a Member</button>
+              <button type="button" className="join">Join Mission</button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 };
 
 export default MissionsPage;
