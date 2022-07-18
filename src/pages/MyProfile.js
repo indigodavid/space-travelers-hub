9f52722ecc97898e43e7e6783c178209449c444a
David Vera Castillo <indigodavid@gmail.com>
2022-07-18 11:52:32 -0500

    Add fields from props received in MyProfile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 const MyProfile = (props) => {
   const { missions, rockets } = props;
@@ -9,7 +9,7 @@ const MyProfile = (props) => {
         <h2>My Missions</h2>
         <ul id="missions-list">
           {missions.map((mission) => (
-            <li>{mission}</li>
+            <li key={mission.mission_id}>{mission.mission_name}</li>
           ))}
         </ul>
       </div>
@@ -17,18 +17,17 @@ const MyProfile = (props) => {
         <h2>My Rockets</h2>
         <ul id="rockets-list">
           {rockets.map((rocket) => (
-            <li>{rocket}</li>
+            <li key={rocket.id}>{rocket.rocket_name}</li>
           ))}
         </ul>
       </div>
     </div>
-  )
+  );
 };
 
 MyProfile.propTypes = {
   missions: PropTypes.string.isRequired,
   rockets: PropTypes.string.isRequired,
-}
-
+};
 
 export default MyProfile;
