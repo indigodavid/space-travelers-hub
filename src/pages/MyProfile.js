6301a073a2f12910ae62110f2d0ee73ccf8e69b4
David Vera Castillo <indigodavid@gmail.com>
2022-07-18 11:44:17 -0500

    Add props to MyProfile component

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const MyProfile = () => (
-  <div className="profile">
-    <div className="profile-column">
-      <h2>My Missions</h2>
-      <ul id="missions-list" />
+const MyProfile = (props) => {
+  const { missions, rockets } = props;
+  return (
+    <div className="profile">
+      <div className="profile-column">
+        <h2>My Missions</h2>
+        <ul id="missions-list">
+          {missions.map((mission) => (
+            <li>{mission}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="profile-column">
+        <h2>My Rockets</h2>
+        <ul id="rockets-list">
+          {rockets.map((rocket) => (
+            <li>{rocket}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-    <div className="profile-column">
-      <h2>My Rockets</h2>
-      <ul id="rockets-list" />
-    </div>
-  </div>
-);
+  )
+};
+
+MyProfile.propTypes = {
+  missions: PropTypes.string.isRequired,
+  rockets: PropTypes.string.isRequired,
+}
+
 
 export default MyProfile;
