ad062564093ebcf1eacf65a78fe6111598cb96bf
David Vera Castillo <indigodavid@gmail.com>
2022-07-18 16:57:46 -0500

    Add store to Provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
