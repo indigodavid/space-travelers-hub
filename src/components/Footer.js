31f870b2630cd37aa334bab9367c0abf50167eef
David Vera Castillo <indigodavid@gmail.com>
2022-07-18 10:29:58 -0500

    Add Logo and attribution with Footer as React component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <a href="https://www.flaticon.com/free-icons/planet" title="planet icons">Planet icons created by Freepik - Flaticon</a>
+  </footer>
+);
+
+export default Footer;
