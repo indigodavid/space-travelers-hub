7ac171bdf0442f3dfeded8deccde7e7de93c3a3e
David Vera Castillo <indigodavid@gmail.com>
2022-07-19 11:58:54 -0500

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
