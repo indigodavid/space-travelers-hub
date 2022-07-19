7ea70967486995b43357f8d1f6970d9227a85bc4
David Vera Castillo <indigodavid@gmail.com>
2022-07-19 14:05:08 -0500

    Create Missions as separate module

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missions';
 
-const Missions = () => (
-  <div>Missions</div>
-);
+const Missions = () => {
+  const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, []);
+  return (
+    <ul className="missionsData">
+      {missions.map((mission) => (
+        <li key={mission.mission_id}>
+          <div className="missionName">{mission.mission_name}</div>
+          <div className="description">{mission.description}</div>
+          <div className="status">
+            <button type="button" className="membership">Not a Member</button>
+            <button type="button" className="join">Join Mission</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Missions;
