b28c8e5fc57bde42ba3a0c4ee2dbdfbd34a46fb7
David Vera Castillo <indigodavid@gmail.com>
2022-07-20 13:32:22 -0500

    Fix Linters errors

--- src/components/__tests__/Missions.test.js
+++ src/components/__tests__/Missions.test.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
-import store from '../../redux/configureStore'
+import store from '../../redux/configureStore';
 import Missions from '../Missions';
-
 
 const renderMissionsComponent = () => {
   const { container } = render(
     <Provider store={store}>
       <Missions />
-    </Provider>
+    </Provider>,
   );
   return container;
-}
+};
 
 describe('Test Missions component', () => {
   test('Match Snapshot', () => {
     expect(renderMissionsComponent().firstChild).toMatchSnapshot();
-  })
+  });
 
   test('Find Elements ny text', () => {
     expect(renderMissionsComponent().querySelector('.missionsData')).toBeInTheDocument();
-  })
-})
-
+  });
+});
--- src/pages/__tests__/MyProfile.test.js
+++ src/pages/__tests__/MyProfile.test.js
@@ -2,27 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
-import store from '../../redux/configureStore'
+import store from '../../redux/configureStore';
 import MyProfile from '../MyProfile';
 
 const renderMyProfilePage = () => {
   const { container } = render(
     <Provider store={store}>
       <MyProfile />
-    </Provider>
+    </Provider>,
   );
   return container;
-}
+};
 
 describe('Test MissionsPage page', () => {
   test('Match Snapshot', () => {
     expect(renderMyProfilePage().firstChild).toMatchSnapshot();
-  })
+  });
 
   test('Find Elements ny text', () => {
     renderMyProfilePage();
     expect(screen.getByText(/My Rockets/i)).toBeInTheDocument();
     expect(screen.getByText(/My Missions/i)).toBeInTheDocument();
-  })
-})
-
+  });
+});
