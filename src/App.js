635552942e32692667fb5af95aaa704cbffce9e6
masangana <alexmasangana@gmail.com>
2022-07-20 22:52:50 +0300

    remove useless files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-//import './App.css';
 import Header from './components/Header';
 import MyProfile from './pages/MyProfile';
 import Rockets from './components/rockects/Rockets';
