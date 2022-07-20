acbe8e4ab619a115e06b5a999f09dd7ec3c10218
David Vera Castillo <indigodavid@gmail.com>
2022-07-20 13:32:22 -0500

    Fix Linters errors

--- src/pages/__tests__/MissionsPage.test.js
+++ src/pages/__tests__/MissionsPage.test.js
@@ -2,32 +2,32 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
-import store from '../../redux/configureStore'
-import MissionsPage from "../MissionsPage";
+import store from '../../redux/configureStore';
+import MissionsPage from '../MissionsPage';
 
 const renderMissionsPage = () => {
   const { container } = render(
     <Provider store={store}>
       <MissionsPage />
-    </Provider>
+    </Provider>,
   );
   return container;
-}
+};
 
 describe('Test MissionsPage page', () => {
   test('Match Snapshot', () => {
     expect(renderMissionsPage().firstChild).toMatchSnapshot();
-  })
+  });
 
   test('Test HTML elements', () => {
     expect(renderMissionsPage().querySelector('.missions')).toBeInTheDocument();
-  })
+  });
 
   test('Find Elements ny text', () => {
     renderMissionsPage();
     expect(screen.getByText(/Status/i)).toBeInTheDocument();
     expect(screen.getByText(/Descriptions/i)).toBeInTheDocument();
-  })
+  });
 
   test('Find buttons', () => {
     renderMissionsPage();
@@ -35,6 +35,5 @@ describe('Test MissionsPage page', () => {
     setTimeout(() => {
       expect(screen.getByText(/Join Mission/i)).toBeInTheDocument();
     }, 1000);
-  })
-})
-
+  });
+});
